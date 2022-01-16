5cd30fbf9cd87e9a89ca87fa772b6c8457c49713
Reaper <ahoy@barelyhuman.dev>
2022-01-17 01:35:01 +0530

    external site meta

--- packages/server/pages/_app.js
+++ packages/server/pages/_app.js
@@ -1,9 +1,11 @@
 import Footer from "components/footer";
+import Head from "components/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head />
       <Component {...pageProps} />
       <Footer />
     </>
